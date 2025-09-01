e8cf226641a516df3eb73476683050df9fcc71ef
svartfika <svartfika@proton.me>
2025-09-01 12:16:16 +0200

    add: prototype for @dlt.source jobsearch_source() with @configspec and config.toml

---
+++ _eda/hugo/dlt_pipeline/jobsearch_pipeline.py
@@ -0,0 +1,68 @@
+import os
+from dataclasses import field
+from pathlib import Path
+from pprint import pprint
+
+import dlt
+import duckdb
+from dlt.sources.config import configspec
+from dlt.sources.helpers.rest_client import RESTClient
+from dlt.sources.helpers.rest_client.paginators import OffsetPaginator
+
+
+@configspec
+class JobsearchConfig:
+    query: str = ""
+    occupation_fields: list[str] = field(default_factory=lambda: [""])
+
+
+@dlt.source
+def jobsearch_source(
+    config: JobsearchConfig = dlt.config.value,
+):
+    client = RESTClient(
+        base_url="https://jobsearch.api.jobtechdev.se/",
+        paginator=OffsetPaginator(limit=100, maximum_offset=200),
+    )
+
+    query = config.query
+    occupation_fields = config.occupation_fields
+
+    @dlt.resource(name="jobads")
+    def get_jobads(query, occupation_fields):
+        for occupation_field in occupation_fields:
+            for page in client.paginate(
+                path="/search",
+                params={"q": query, "occupation-field": occupation_field},
+            ):
+                response_json = page.response.json()
+
+                for jobad in response_json.get("hits", []):
+                    yield jobad
+
+    yield get_jobads(query, occupation_fields)
+
+
+if __name__ == "__main__":
+    os.chdir(Path(__file__).parent)
+
+    db = duckdb.connect(":memory:")
+    p = dlt.pipeline(
+        pipeline_name="jobsearch",
+        destination=dlt.destinations.duckdb(db),
+        dataset_name="jobads",
+        # dev_mode=True,
+    )
+
+    dlt_loadinfo = p.run(jobsearch_source())
+
+    print(dlt_loadinfo)
+
+    print("\n", "Loaded Occupational Fields:")
+    pprint(
+        db.sql(
+            """
+        select distinct occupation_field__label, occupation_field__concept_id from jobads;
+        """
+        ).fetchall()
+    )
