37ea6a49472439d378a3888498722c39aba77d3a
svartfika <svartfika@proton.me>
2025-09-14 20:53:46 +0200

    change: capitalize_first_letter on select columns

--- dbt_code/models/src/src_employer.sql
+++ dbt_code/models/src/src_employer.sql
@@ -5,9 +5,9 @@ select
     employer__name as employer_name,
     employer__workplace as employer_workplace,
     employer__organization_number as employer_organization_number,
-    workplace_address__street_address as workplace_street_address,
-    workplace_address__region as workplace_region,
+    {{ capitalize_first_letter('workplace_address__street_address') }} as workplace_street_address,
     workplace_address__postcode as workplace_postcode,
-    workplace_address__city as workplace_city,
+    {{ capitalize_first_letter('workplace_address__city') }} as workplace_street_address,
+    workplace_address__region as workplace_region,
     workplace_address__country as workplace_country
 from stg_job_ads
