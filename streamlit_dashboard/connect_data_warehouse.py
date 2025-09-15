f30f7459b89ab2d44ae887bd25fb20228dad78a8
svartfika <svartfika@proton.me>
2025-09-15 18:35:54 +0200

    add: status messages, docstring

--- streamlit_dashboard/connect_data_warehouse.py
+++ streamlit_dashboard/connect_data_warehouse.py
@@ -1,15 +1,16 @@
 import os
-from dotenv import load_dotenv
+
+import pandas as pd
 import snowflake.connector
-import pandas as pd 
-
- 
-def query_job_listings(mart_table: str ) -> pd.DataFrame:
+from dotenv import load_dotenv
 
 
-    query=f'SELECT * FROM {mart_table}'
+def query_job_listings(mart_table: str) -> pd.DataFrame:
+    """Queries a job listings table and returns the data as a pandas DataFrame."""
 
     load_dotenv()
+
+    print("Initializing connection to Snowflake...")
 
     with snowflake.connector.connect(
         user=os.getenv("SNOWFLAKE_USER"),
@@ -20,8 +21,12 @@ def query_job_listings(mart_table: str ) -> pd.DataFrame:
         schema=os.getenv("SNOWFLAKE_SCHEMA"),
         role=os.getenv("SNOWFLAKE_ROLE"),
     ) as conn:
+        query = f"SELECT * FROM {mart_table}"
 
-        # Execute the query
+        print(f"Executing query and fetching data for '{mart_table}'...")
+
         df = pd.read_sql(query, conn)
+
+        print(f"Successfully fetched {len(df)} rows into a Pandas DataFrame.")
 
         return df
