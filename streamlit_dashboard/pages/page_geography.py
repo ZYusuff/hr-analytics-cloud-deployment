74768671602f510e27c2c40717df82c6e7ca5352
svartfika <svartfika@proton.me>
2025-09-16 11:24:16 +0200

    add: test query

--- streamlit_dashboard/pages/page_geography.py
+++ streamlit_dashboard/pages/page_geography.py
@@ -8,10 +8,27 @@ con = create_job_listings_db("mart_geography", duckdb_table_name="db_mart")
 selected_occupation_field = st.session_state.occupation_field_filter
 
 if selected_occupation_field == "All":
-    rel_filtered = con.sql("select * from db_mart")
+    rel = con.sql("select * from db_mart")
 else:
-    rel_filtered = con.sql(
+    rel = con.sql(
         "select * from db_mart where OCCUPATION_FIELD = ?",
         params=[selected_occupation_field],
     )
 
+# -- create datasets
+
+rel_field_country_total = con.sql(
+    """
+    select
+        location_key AS country_code,
+        location_display_name,
+        total_vacancies,
+        occupation_display_name
+    from rel
+    where
+        location_level = 'country'
+        and occupation_level = 'field'
+"""
+)
+
+st.dataframe(rel_field_country_total.df())
