abd58de16dfdc100e60a8b5eddc453014c458581
Masoud Abdisaran <masoudabdi@hotmail.com>
2025-09-18 11:06:09 +0200

    page_browser dashboard

--- streamlit_dashboard/connect_data_warehouse.py
+++ streamlit_dashboard/connect_data_warehouse.py
@@ -1,5 +1,4 @@
 import os
-
 import duckdb
 import snowflake.connector
 from dotenv import load_dotenv
@@ -72,3 +71,7 @@ def create_job_listings_db(
     print(f"Successfully registered {tbl_mart.num_rows} rows into DuckDB as table '{duckdb_table_name}'.")
 
     return duck_conn
+
+if __name__ == "__main__":
+    df = query_job_listings("marts.job_listings")
+    print(df.head())  # Visa de första raderna
--- streamlit_dashboard/pages/homepage.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-
-st.title("🏠 Welcome to the Dashboard")
-st.write("This is the homepage of your Streamlit dashboard.")
-st.info("Use the sidebar to navigate between pages.")
