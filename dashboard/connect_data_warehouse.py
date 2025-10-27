1e721cce5cbec38dd52c1d8c35e69c3fce710c81
Masoud Abdisaran <masoudabdi@hotmail.com>
2025-10-27 15:23:27 +0100

    update dockerfile.dashboard, compose and connect_data_warehouse.py

--- dashboard/connect_data_warehouse.py
+++ dashboard/connect_data_warehouse.py
@@ -2,8 +2,9 @@ from pathlib import Path
 import duckdb
 
 # data warehouse directory
-db_path = str(Path(__file__).parents[1] / "data_warehouse/job_ads.duckdb")
+FILE_SHARE_PATH = Path("/mnt/data/job_ads.duckdb")
  
+
 def query_job_listings(query='SELECT * FROM marts.mart_technical_jobs'):
     with duckdb.connect(db_path, read_only=True) as conn:
         return conn.query(f"{query}").df()
